2f1f5f8ac8159de2fcf68c3980329822adbb5fc7
rayspock <ray@rayspock.com>
2024-01-28 17:27:18 +0000

    Migrate to Manifest V3

--- src/js/content.js
+++ src/js/content.js
@@ -1,14 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === "NOTIFY_CLEAR_DATA") {
-    chrome.storage.sync.get(['reload', 'noPrompt', 'domain'], (data) => {
+    chrome.storage.sync.get(["reload", "noPrompt", "domain"], (data) => {
       if (data.noPrompt) {
         chrome.runtime.sendMessage({ command: "CLEAR_DATA" }, (response) => {
-          console.debug("Sending CLEAR_DATA message: ", response)
+          console.debug("Sending CLEAR_DATA message: ", response);
         });
       } else {
         Swal.fire({
           title: "Clear site data?",
-          text: "All data stored by " + data.domain + " and any sites under it will be deleted. This includes cookies. You'll be signed out of these sites, including in open tabs.",
+          text:
+            "All data stored by " +
+            data.domain +
+            " and any sites under it will be deleted. This includes cookies. You'll be signed out of these sites, including in open tabs.",
           icon: "warning",
           showCancelButton: true,
           confirmButtonColor: "#3085d6",
@@ -16,30 +19,29 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           confirmButtonText: "Yes, delete it!",
         }).then((result) => {
           if (result.value) {
-            chrome.runtime.sendMessage({ command: "CLEAR_DATA" }, (response) => {
-              console.debug("Sending CLEAR_DATA message: ", response)
-            });
+            chrome.runtime.sendMessage(
+              { command: "CLEAR_DATA" },
+              (response) => {
+                console.debug("Sending CLEAR_DATA message: ", response);
+              },
+            );
           }
         });
       }
     });
   } else if (request.command === "CLEAR_DATA_SUCCESS") {
-    chrome.storage.sync.get(['reload', 'noPrompt', 'domain'], (data) => {
+    chrome.storage.sync.get(["reload", "noPrompt", "domain"], (data) => {
       if (data.reload) {
         history.go(0);
       } else {
         if (!data.noPrompt) {
-          Swal.fire(
-            'Deleted!',
-            'Your cookies has been deleted.',
-            'success'
-          )
+          Swal.fire("Deleted!", "Your cookies has been deleted.", "success");
         }
       }
     });
   } else {
     sendResponse({ result: "Unknown command:" + request.command });
-    return
+    return;
   }
   sendResponse({ result: "success" });
 });
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,11 +1,23 @@
-export function notifyClearData(chrome, tab) {
-    let url = new URL(tab.url);
-    chrome.storage.sync.set({'domain': "https://" + url.hostname})
+export const notifyClearData = (chrome, tab) => {
+  let url = new URL(tab.url);
+  chrome.storage.sync.set({ domain: "https://" + url.hostname });
+  chrome.tabs.sendMessage(
+    tab.id,
+    { command: "NOTIFY_CLEAR_DATA" },
+    (response) => {
+      console.debug("Sending NOTIFY_CLEAR_DATA message:", response.result);
+    },
+  );
+};
+
+export const notifyClearDataSuccess = (chrome) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(
-      tab.id,
-      { command: "NOTIFY_CLEAR_DATA" },
+      tabs[0].id,
+      { command: "CLEAR_DATA_SUCCESS" },
       (response) => {
-        console.debug("Sending NOTIFY_CLEAR_DATA message:", response.result)
-      }
+        console.debug("Sending CLEAR_DATA_SUCCESS message:", response.result);
+      },
     );
-}
+  });
+};
